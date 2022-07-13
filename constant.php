64cbe7be3d07b647fae411e5ef231758e8f07ced
Amarkumar20 <109210152+Amarkumar20@users.noreply.github.com>
2022-07-13 15:48:35 +0530

    Add files via upload

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6LdyF90ZAAAAAJinwmnhSNjOzOi2agM-SdQSljNF"); 
+define('SECRET_KEY',"6LdyF90ZAAAAAKdOAFVFU-FsNJ4g11VJfv705DxM")
+?>
